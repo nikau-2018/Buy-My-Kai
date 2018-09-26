38c48e1ab1137f5e4416711c67fd213af17c6185
Amy Han <syamyhan@gmail.com>
2018-09-27 11:46:18 +1200

    app component done again

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 
 import './app.css'
 import LoginScreen from '../LoginScreen'
+import Login from '../Login'
 import RegisterScreen from '../RegisterScreen'
 import AreaScreen from '../AreaScreen'
 import Area from '../Area'
@@ -13,6 +14,7 @@ export default class App extends Component {
     return (
       <div className="app">
         <Route exact path='/' component={LoginScreen} />
+        <Route exact path='/Login' component={Login} />
         <Route path='/register' component={RegisterScreen} />
         <Route path='/neighbourhood' component={AreaScreen} />
         <Route path='/neighbourhood/?query' component={Area} />

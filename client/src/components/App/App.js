708a065b69dad33a87920ef79b7b1560cbdcb5d2
Amy Han <syamyhan@gmail.com>
2018-09-27 11:29:30 +1200

    app component done

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,29 +1,22 @@
-import React, {Component} from 'react'
-import {Route} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 
 import './app.css'
-import LogInScreen from '../LogInScreen'
+import LoginScreen from '../LoginScreen'
 import RegisterScreen from '../RegisterScreen'
 import AreaScreen from '../AreaScreen'
 import Area from '../Area'
-
-import {Registration, Update} from '../GrowerForm'
 import ProfileScreen from '../ProfileScreen'
 
 export default class App extends Component {
   render () {
     return (
       <div className="app">
-        <Route exact path='/' component={LogInScreen} />
+        <Route exact path='/' component={LoginScreen} />
         <Route path='/register' component={RegisterScreen} />
         <Route path='/neighbourhood' component={AreaScreen} />
         <Route path='/neighbourhood/?query' component={Area} />
-
-        {/* Routes for growers list */}
-        {/* Routes for loading images */}
-        <Route path='/images' component={Registration} />
         <Route path='/profile/:id' component={ProfileScreen} />
-        <Route path='/update' component={Update} />
       </div>
     )
   }

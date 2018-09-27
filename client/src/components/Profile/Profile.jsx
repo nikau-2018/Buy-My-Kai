72a4463a8592a84d25e5f3ee22a25dc2bec5ae73
Lauren Hart <laurenannehart@gmail.com>
2018-09-28 11:49:08 +1200

    started profile component and gone for burgerrrr

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,17 +1,68 @@
-// import React, {Component} from 'react'
-// // import from data
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
-// const Profile = (props) => {
-//   <div className='container'>
-//     <div className='profile'>
-//       {Profile.map((profile) => {
-//         return (
-//           <Profile
-//             name={profile.name}
-//             email={profile.email}
-//             address={profile.address} />
-//         )
-//       })}
-//     </div>
-//   </div>
-// }
+import {getProfile} from '//action'
+import {addProduct} from '//action'
+
+class Profile extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getProfile())
+}
+
+  render () {
+    const profile = this.props
+    return (
+      <div className='profile' >
+        <div className='name'>
+          {profile.name}
+        </div>
+
+        <div className='email'>
+          {profile.email}
+        </div>
+
+        <div className='description'>
+          {profile.description}
+        </div>
+
+        <div className='address'>
+          {profile.address}
+        </div>
+
+        <div className='suburb'>
+          {profile.suburb}
+        </div>
+
+        <div className='city'>
+          {profile.city}
+        </div>
+
+        <div className='postcode'>
+          {profile.postcode}
+        </div>
+
+        <div className='hours'>
+          {profile.hours}
+        </div>
+
+        <button className='addproduct'>
+          <Link to={'/addproduct'}>Add Product</Link>
+        </button >
+
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    profile: state.profile
+  }
+}
+
+export default connect(mapStateToProps)(Profile)

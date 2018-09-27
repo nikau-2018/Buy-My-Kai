31ac613a567485be7514c8dc8dc2f2954fbd3c88
Lauren Hart <laurenannehart@gmail.com>
2018-09-27 15:10:53 +1200

    added empty states

---
+++ client/src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+// import React, {Component} from 'react'
+// // import from data
+
+// const Profile = (props) => {
+//   <div className='container'>
+//     <div className='profile'>
+//       {Profile.map((profile) => {
+//         return (
+//           <Profile
+//             name={profile.name}
+//             email={profile.email}
+//             address={profile.address} />
+//         )
+//       })}
+//     </div>
+//   </div>
+// }

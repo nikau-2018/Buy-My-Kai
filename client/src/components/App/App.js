9f9239ca63e1bd671a9227be810ecc452391df0b
emma-nio <emmanioashley@gmail.com>
2018-09-27 13:33:50 +1200

    removed dublicate components, added new routes and Profile folder

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Route} from 'react-router-dom'
 
 import './app.css'
-import LoginScreen from '../LoginScreen'
+import Home from '../Home'
 import Login from '../Login'
-import RegisterScreen from '../RegisterScreen'
+import Register from '../Register'
 import AreaScreen from '../AreaScreen'
 import Area from '../Area'
-import ProfileScreen from '../ProfileScreen'
+import Profile from '../Profile'
+import AddProduct from '../Product'
 
 export default class App extends Component {
   render () {
     return (
       <div className="app">
-        <Route exact path='/' component={LoginScreen} />
-        <Route exact path='/Login' component={Login} />
-        <Route path='/register' component={RegisterScreen} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route path='/register' component={Register} />
         <Route path='/neighbourhood' component={AreaScreen} />
         <Route path='/neighbourhood/?query' component={Area} />
-        <Route path='/profile/:id' component={ProfileScreen} />
+        <Route path='/profile/:id' component={Profile} />
+        <Route path='/profile/:id/addProduct' component={AddProduct}/>
       </div>
     )
   }

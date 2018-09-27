3b7dec95b2e3c6ec52a4de01c2f162669eb6ddb2
Amy Han <syamyhan@gmail.com>
2018-09-27 13:48:04 +1200

    Update Home component

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+import Login from '../Login'
+
+export default class Home extends Component {
+  render () {
+    return (
+      <div className='container'>
+        <Login />
+      </div>
+    )
+  }
+}

2edbf3fa48edf37ce22c577f8642531128e98694
Lisa <zuzatoy@gmail.com>
2018-09-27 16:54:43 +1200

    fix login

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -5,23 +5,23 @@ import {Route} from 'react-router-dom'
 import './app.css'
 import Home from '../Home'
 import Login from '../Login/Login'
-import Register from '../Register/Register'
-import AreaScreen from '../Area/AreaScreen'
-import Area from '../Area/Area'
-import Profile from '../Profile'
-import AddProduct from '../Product'
+// import Register from '../Register/Register'
+// import AreaScreen from '../Area/AreaScreen'
+// import Area from '../Area/Area'
+// import Profile from '../Profile'
+// import AddProduct from '../Product'
 
 export default class App extends Component {
   render () {
     return (
       <div className="app">
         <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+        {/* <Route path='/register' component={Register} />
         <Route path='/neighbourhood' component={AreaScreen} />
         <Route path='/neighbourhood/?query' component={Area} />
         <Route path='/profile/:id' component={Profile} />
-        <Route path='/profile/:id/addProduct' component={AddProduct}/>
+        <Route path='/profile/:id/addProduct' component={AddProduct}/> */}
       </div>
     )
   }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import {combineReducers} from 'redux'
 
 import currentUser from './currentUser'
+import loginReducers from './login'
 
 export default combineReducers({
-  currentUser
+  currentUser,
+  loginReducers
+
 })

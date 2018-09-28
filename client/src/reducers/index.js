13fcdf7d228362b0c974416f216ace1b468744b0
Lauren Hart <laurenannehart@gmail.com>
2018-09-28 15:18:32 +1200

    added areaReducer to the reducer combiner

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import currentUser from './currentUser'
 import loginReducers from './login'
 import registerReducer from './register'
 import addproductReducer from './addproduct'
+import areaReducer from './areaReducer'
 
 export default combineReducers({
   currentUser,
   loginReducers,
   registerReducer,
-  addproductReducer
+  addproductReducer,
+  areaReducer
 })

54d484ac176214245a52db32b67e2c8cd75850f9
Lauren Hart <laurenannehart@gmail.com>
2018-09-28 12:57:06 +1200

    added profileReducer to reducers index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import {combineReducers} from 'redux'
 import currentUser from './currentUser'
 import loginReducers from './login'
 import registerReducer from './register'
+import profileReducer from './profile'
 
 export default combineReducers({
   currentUser,
   loginReducers,
-  registerReducer
+  registerReducer,
+  profileReducer
 })

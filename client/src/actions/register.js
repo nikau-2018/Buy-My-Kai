1774cfe785b4f119999c9819e830d63c34b434e6
Lisa <zuzatoy@gmail.com>
2018-09-29 16:00:44 +1200

    lat and long to DB

--- client/src/actions/register.js
+++ client/src/actions/register.js
@@ -4,6 +4,8 @@ export const SHOW_ERROR = 'SHOW_ERROR'
 export const REGISTER_PENDING = 'REGISTER_PENDING'
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 
+const GEOCODING_PROVIDER_URL = 'http://www.mapquestapi.com/geocoding/v1/address?key=4d6Splj1DnO9rnsmLbDkjAuyqmExW4KH';
+
 export const showError = (errorMessage) => {
   return {
     type: SHOW_ERROR,
@@ -27,18 +29,32 @@ export const registerSuccess = user => {
 export function postUser (user) {
   return (dispatch) => {
     dispatch(registerPending())
-    return request
-      .post('/api/v1/users/register', user)
-      .then(res => {
-        // dispatch registerSuccess.
+    const searchAddress = `${user.address} ${user.suburb} ${user.city} New Zealand`;
+    console.log(searchAddress);
 
-        dispatch(registerSuccess(res.data.user))
-
-        // eslint-disable-next-line no-console
-        console.log('success')
-      })
-      .catch(err => {
-        dispatch(showError(err.message))
-      })
+    return getLatLng(searchAddress)
+    .then(({ lat, lng: long}) => {
+      const userWithCoordinates = {
+        ...user,
+        lat,
+        long,
+      }
+      console.log(userWithCoordinates)
+      request.post('/api/v1/users/register', userWithCoordinates);
+    })
+    .then((result) => {
+      console.log(result)
+      // dispatch(registerSuccess(result.data.user))
+    })
+    .catch((err) => {
+      dispatch(showError(err.message))
+    })
   }
 }
+
+
+export function getLatLng (address) {
+  return request.post(GEOCODING_PROVIDER_URL, { location: address })
+    .then((response) => response.data.results[0].locations[0].latLng)
+    .catch((error) => console.log(error));
+};

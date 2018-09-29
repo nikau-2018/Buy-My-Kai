5fb7aa395c65a8171595c198d64e26c236d7da2d
Lauren Hart <laurenannehart@gmail.com>
2018-09-30 09:29:40 +1300

    refactoring

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {postUser} from '../../actions/register'
-import { TextField, Button, Checkbox } from '@material-ui/core';
+import {TextField, Button, Checkbox} from '@material-ui/core'
 
 import styles from '../../styles/styles.css'
 import logo from '../../images/Logo.png'
@@ -75,110 +75,109 @@ class Register extends React.Component {
         <div className="register-container pure-u-1-1 pure-u-md-1-2">
           <img className="pure-img logo" src={logo}/>
           <h2>Sign up</h2>
-          
-            <TextField 
-              type="text" 
-              label="Name" 
-              name='name' 
-              margin="normal"
-              value={this.state.name} 
-              onChange={this.handleChange} /><br />
 
-            <TextField 
-              type="email" 
-              label="Email Address" 
-              name='email' 
-              margin="normal"
-              value={this.state.email} 
-              onChange={this.handleChange} /><br />
+          <TextField
+            type="text"
+            label="Name"
+            name='name'
+            margin="normal"
+            value={this.state.name}
+            onChange={this.handleChange} /><br />
 
-            <TextField 
-              type="password" 
-              label="Password" 
-              name='hash' 
-              margin="normal"
-              value={this.state.hash} 
-              onChange={this.handleChange} /><br />
+          <TextField
+            type="email"
+            label="Email Address"
+            name='email'
+            margin="normal"
+            value={this.state.email}
+            onChange={this.handleChange} /><br />
 
-            {
-              this.state.isSeller
-              ? 
-              <div>
-                <TextField 
-                  type="text" 
+          <TextField
+            type="password"
+            label="Password"
+            name='hash'
+            margin="normal"
+            value={this.state.hash}
+            onChange={this.handleChange} /><br />
+
+          {
+            this.state.isSeller
+              ? <div>
+                <TextField
+                  type="text"
                   label="Street Address"
                   multiline
                   rowsMax="4"
-                  name='address' 
+                  name='address'
                   margin="normal"
-                  value={this.state.address} 
-                  onChange={this.handleChange} /><br />
-
-                <TextField 
-                  type="text" 
-                  name='suburb' 
-                  label="Suburb"
-                  margin="normal"
-                  value={this.state.suburb} 
-                  onChange={this.handleChange} /><br />
-
-                <TextField 
-                  type="text" 
-                  name='city' 
-                  label="City"
-                  margin="normal"
-                  value={this.state.city} 
+                  value={this.state.address}
                   onChange={this.handleChange} /><br />
 
                 <TextField
-                  type="text" 
-                  name='postcode' 
+                  type="text"
+                  name='suburb'
+                  label="Suburb"
+                  margin="normal"
+                  value={this.state.suburb}
+                  onChange={this.handleChange} /><br />
+
+                <TextField
+                  type="text"
+                  name='city'
+                  label="City"
+                  margin="normal"
+                  value={this.state.city}
+                  onChange={this.handleChange} /><br />
+
+                <TextField
+                  type="text"
+                  name='postcode'
                   label="Post Code"
                   margin="normal"
                   value={this.state.postcode}
                   onChange={this.handleChange} /><br />
 
-                  <TextField
-                    type="text"
-                    name='description' 
-                    multiline
-                    rowsMax="8"
-                    label="Description"
-                    margin="normal"
-                    value={this.state.description} 
-                    onChange={this.handleChange} /><br />
+                <TextField
+                  type="text"
+                  name='description'
+                  multiline
+                  rowsMax="8"
+                  label="Description"
+                  margin="normal"
+                  value={this.state.description}
+                  onChange={this.handleChange} /><br />
 
-                  <TextField
-                    type="text" 
-                    name='hours' 
-                    label="Hours"
-                    margin="normal"
-                    value={this.state.hours} 
-                    onChange={this.handleChange} /><br />
+                <TextField
+                  type="text"
+                  name='hours'
+                  label="Hours"
+                  margin="normal"
+                  value={this.state.hours}
+                  onChange={this.handleChange} /><br />
 
               </div>
               : <div></div>
-            }
-            <div>
-              <Checkbox type='checkbox' 
-              checked={this.state.isClicked} 
-              name='seller' 
-              onClick={this.handleSeller} 
+          }
+          <div>
+            <Checkbox type='checkbox'
+              checked={this.state.isClicked}
+              name='seller'
+              onClick={this.handleSeller}
               onChange={this.handleChange} />
-              <span>Are you a seller?</span>
+            <span>Are you a seller?</span>
 
-              {this.state.isSeller ? 
-                <Button 
-                className='btn btn--primary' 
-                onClick={this.sendUser}>Complete Sign Up</Button> : 
-                
-                <Button 
-                className="btn btn--primary" 
+            {this.state.isSeller
+              ? <Button
+                className='btn btn--primary'
+                onClick={this.sendUser}>Complete Sign Up</Button>
+
+              : <Button
+                className="btn btn--primary"
                 onClick={this.sendUser}>Go</Button>
-              }
-              {this.state.success && <Redirect to="/neighbourhood"/>}
-            </div>
-          
+            }
+            {this.state.success && <Redirect to="/neighbourhood"/>}
+          </div>
+
         </div>
       </div>
     )

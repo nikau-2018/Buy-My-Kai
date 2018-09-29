ac76c30da8d3df3562cc030c97368179a55a0bcf
Lauren Hart <laurenannehart@gmail.com>
2018-09-29 16:35:08 +1200

    dummy seeds for users and products

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { Button } from '@material-ui/core';
+import { Button } from '@material-ui/core'
 
 import styles from '../../styles/styles.css'
 import logo from '../../images/Logo.png'

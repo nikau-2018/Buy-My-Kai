497f3490343da4d8a253394ba3a7f06e559a6f1e
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-09-30 16:33:04 +1300

    backup map with markers

--- client/src/components/Area/Area.jsx
+++ client/src/components/Area/Area.jsx
@@ -40,30 +40,37 @@ class Area extends React.Component {
     const growersList = this.props.growersList || [];
     // console.log('jsx:', this.props.growersList)
     return (
-      <div>
+      <div className='container'>
         <h1>Search For Growers</h1>
         <input type="text" name='suburb' value={this.state.suburb} placeholder='Suburb' onChange={this.handleChange} /><br />
         <button onClick={this.handleClick}>search</button>
         <div>{growersList.map(list =>
           <List key={list.id} list={list} />
         )}</div>
-        <Map className="Leaflet" style={{ position: 'absolute' }} center={[-36.848461, 174.763336]} zoom={15}>
-          <TileLayer
-            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <div>{growersList.map(x => console.log(x, 33) ||
-            <Marker position={[x.id]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-            </Marker>
-          )
-        }
-          )}
 
-          </div>
-        </Map>
+        <div className='map'>
+          <Map center={[-36.848461, 174.763336]} zoom={12}>
+            <TileLayer
+              attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <div className='marker'>
+              <Marker className='belmont' position={[-36.80506470, 174.78860850]}>
+                <Popup><button>Belmont</button></Popup>
+              </Marker>
+              <Marker className='takapuna' position={[-36.78792290, 174.76882070]}>
+                <Popup><button>Takapuna</button></Popup>
+              </Marker>
+              <Marker className='newmarket' position={[-36.86983670, 174.77758010]}>
+                <Popup><button onClick={this.handleClick} value={this.state.suburb} onChange={this.handleChange}>New Market</button>
+                </Popup>
+              </Marker>
+              <Marker className='ponsonby' position={[-36.84877790, 174.73806630]}>
+                <Popup><button>Ponsonby</button></Popup>
+              </Marker>
+            </div>
+          </Map>
+        </div>
       </div>
     )
   }

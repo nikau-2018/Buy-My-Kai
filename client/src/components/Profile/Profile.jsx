71ff31e4c28edb67ff21e953c87d23378cbdda74
Zaine Kingi <rous6026nz@gmail.com>
2018-09-30 18:19:50 +1300

    Remove getHeaders call from all actions the dont require.

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,13 +4,14 @@ import {Link} from 'react-router-dom'
 
 import {getProfile} from '../../actions/profile'
 import {addProduct} from '../../actions/profile'
+import styles from './styles.css'
 
 class Profile extends React.Component {
 
   render() {
     const profile = this.props.user && this.props.user
     return (
-      <div className='profile pure-u-1'>
+      <div className='profile pure-u-1-2'>
         <h3>Welcome, {profile.name}</h3>
         <ul>
           <li><p>Email: {profile.email}</p></li>
@@ -25,7 +26,6 @@ class Profile extends React.Component {
         <button className='addproduct'>
           <Link to={'/profile/:id/addproduct'}>Add Product</Link>
         </button >
-  
       </div>
     )
   }

8f21914b17585749b6862c75d7137c9b07124bc6
Emma Ashley <emmanioashley@gmail.com>
2018-09-30 17:33:36 +1300

    markers with the list of sellers

--- client/src/components/Area/Area.jsx
+++ client/src/components/Area/Area.jsx
@@ -1,34 +1,37 @@
 import React from 'react'
 // import {Redirect} from 'react-router-dom'
-import { sendNeighbourhood } from '../../actions/area'
-import { connect } from 'react-redux'
+import {sendNeighbourhood} from '../../actions/area'
+import {connect} from 'react-redux'
 import List from './List'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
 
-import './styles.css';
+import './styles.css'
 
 class Area extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       suburb: ''
     }
-    this.handleChange = this.handleChange.bind(this)
+    // this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick() {
-    this.sendNeighbourhood(this.state.suburb)
-  }
-
-  handleChange(e) {
-    e.preventDefault()
+  handleClick (e) {
     this.setState({
       [e.target.name]: e.target.value
     })
+    this.sendNeighbourhood(this.state.suburb)
   }
 
-  sendNeighbourhood() {
+  // handleChange (e) {
+  //   e.preventDefault()
+  //   this.setState({
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
+
+  sendNeighbourhood () {
     this.props.dispatch(sendNeighbourhood(this.state))
     // .then(setTimeout(this.setState({
     //   ready: true
@@ -36,17 +39,16 @@ class Area extends React.Component {
     // ))
   }
 
-  render() {
-    const growersList = this.props.growersList || [];
+  render () {
     // console.log('jsx:', this.props.growersList)
     return (
       <div className='container'>
         <h1>Search For Growers</h1>
-        <input type="text" name='suburb' value={this.state.suburb} placeholder='Suburb' onChange={this.handleChange} /><br />
-        <button onClick={this.handleClick}>search</button>
-        <div>{growersList.map(list =>
+        {/* <input type="text" name='suburb' value={this.state.suburb} placeholder='Suburb' onChange={this.handleChange} /><br /> */}
+        {/* <button onClick={this.handleClick}>search</button>
+        <div>{this.props.growersList && this.props.growersList.map(list =>
           <List key={list.id} list={list} />
-        )}</div>
+        )}</div> */}
 
         <div className='map'>
           <Map center={[-36.848461, 174.763336]} zoom={12}>
@@ -56,13 +58,22 @@ class Area extends React.Component {
             />
             <div className='marker'>
               <Marker className='belmont' position={[-36.80506470, 174.78860850]}>
-                <Popup><button>Belmont</button></Popup>
+                <Popup><button name='suburb'
+                  onClick={this.handleClick} value='Belmont'>
+                  <div>
+                    {this.props.growersList &&
+                    this.props.growersList.map(list =>
+                      <li key={list.id}>{list.name}</li>
+                    )}</div>Belmont</button></Popup>
               </Marker>
               <Marker className='takapuna' position={[-36.78792290, 174.76882070]}>
                 <Popup><button>Takapuna</button></Popup>
               </Marker>
-              <Marker className='newmarket' position={[-36.86983670, 174.77758010]}>
-                <Popup><button onClick={this.handleClick} value={this.state.suburb} onChange={this.handleChange}>New Market</button>
+              <Marker className='mteden' position={[-36.86983670, 174.77758010]}>
+                <Popup><button name='suburb' onClick={this.handleClick} value='Mt Eden'>
+                  <div>{this.props.growersList && this.props.growersList.map(list =>
+                    <p key={list.id}>{list.name}</p>
+                  )}</div></button>
                 </Popup>
               </Marker>
               <Marker className='ponsonby' position={[-36.84877790, 174.73806630]}>
@@ -75,7 +86,6 @@ class Area extends React.Component {
     )
   }
 }
-
 
 const mapStateToProps = (state) => {
   return {

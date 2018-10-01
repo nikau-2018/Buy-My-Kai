071cf0ed186994b21fc5fdcb6631e80d914018dc
Amy Han <40330174+amy-han@users.noreply.github.com>
2018-10-02 09:45:09 +1300

    nav button link working

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -27,71 +27,63 @@ class Profile extends React.Component {
       <div className='profile'>
         <div className="pure-img background"></div>
 
-        <div className='container pure-u-1'>
-          {/* <a href="#" class="pure-menu-heading pure-menu-link">BRAND</a> */}
-          {/* <ul class="pure-menu-list"> */}
-          <div className="btn-group pure-u-1">
-            <Link to="/login">
-              <Button
-                className='btn btn--link'>
+        <div className='container pure-u-1-1 pure-u-md-1-2'>
+
+          <div className="pure-form btn-group btn-nav">
+            <Link to="/profile">
+              <Button>
+                <i className="far fa-user fa-3x"></i>
+              </Button>
+            </Link>
+            <Link to="/neighbourhood">
+              <Button>
                 <i className="far fa-map fa-3x"></i>
               </Button>
             </Link>
-            <div className="btn-group pure-u-1">
-              <Link to="/neighbourhood">
-                <Button
-                  className='btn btn--link'>
-                  <i className="far fa-user fa-3x"></i>
-                </Button>
-              </Link>
-            </div>
-            {/* <li class="pure-menu-item"><Button><i class="far fa-user fa-3x"></i></Button></li>
-            <li class="pure-menu-item"><a href="/neighbourhood"><i class="far fa-map fa-3x"></i></a></li> */}
-            {/* </ul> */}
           </div>
-        </div>
 
-        <div className='container pure-u-1'>
-          <h3>Kia ora Grower</h3>
-          <h5>Thank you for registering with Buy My Kai <br />
-            to share your fruit and veg with your community!</h5>
-          <p>Here you will find your registered details, <br />
-            please make sure they are up to date as this is what<br />
-            Eaters will be seeing when they search your area</p>
-          <ul className="profile-info pure-u-1">
-            {profile.isSeller
-              ? <div>
-                <li><p><strong>Seller Information:</strong><br /> {profile.description}</p></li>
-                <li><p><strong>Address:</strong> {profile.address}</p></li>
-                <li><p><strong>Suburb:</strong> {profile.suburb}</p></li>
-                <li><p><strong>City:</strong> {profile.city}</p></li>
-                <li><p><strong>Postcode:</strong> {profile.postcode}</p></li>
-                <li><p><strong>Availability:</strong> {profile.hours}</p></li>
-                <Button
-                  className="btn--fab"
-                  variant="extendedFab">
-                  <i className="fas fa-plus"></i>
-                </Button >
-              </div>
-              : <div>
-                <li><p><strong>Email:</strong> {profile.email}</p></li>
-                <li>
+          <div className='profile-info pure-u-1-1 pure-u-md-1-2'>
+            <h3>Kia ora Grower</h3>
+            <h5>Thank you for registering with Buy My Kai <br />
+              to share your fruit and veg with your community!</h5>
+            <p>Here you will find your registered details, <br />
+              please make sure they are up to date as this is what<br />
+              Eaters will be seeing when they search your area</p>
+            <ul className="profile-info pure-u-1">
+              {profile.isSeller
+                ? <div>
+                  <li><p><strong>Seller Information:</strong><br /> {profile.description}</p></li>
+                  <li><p><strong>Address:</strong> {profile.address}</p></li>
+                  <li><p><strong>Suburb:</strong> {profile.suburb}</p></li>
+                  <li><p><strong>City:</strong> {profile.city}</p></li>
+                  <li><p><strong>Postcode:</strong> {profile.postcode}</p></li>
+                  <li><p><strong>Availability:</strong> {profile.hours}</p></li>
                   <Button
                     className="btn--fab"
-                    variant="extendedFab"
-                  >
-                    <i className="fas fa-user-check"></i>
-                  </Button>
-                  <span className="fab-label--right">$$tart your own side hustle - Become a $eller today!</span>
-                </li>
-              </div>
-            }
-          </ul>
-          <div>
-            {this.state.showForm ? <Addproduct /> : <div></div>}
-          </div>
-          :
+                    variant="extendedFab">
+                    <i className="fas fa-plus"></i>
+                  </Button >
+                </div>
+                : <div>
+                  <li><p><strong>Email:</strong> {profile.email}</p></li>
+                  <li>
+                    <Button
+                      className="btn--fab"
+                      variant="extendedFab"
+                    >
+                      <i className="fas fa-user-check"></i>
+                    </Button>
+                    <span className="fab-label--right">$$tart your own side hustle - Become a $eller today!</span>
+                  </li>
+                </div>
+              }
+            </ul>
+            <div>
+              {this.state.showForm ? <Addproduct /> : <div></div>}
+            </div>
+            :
       </div>
+        </div>
       </div>
     )
   }

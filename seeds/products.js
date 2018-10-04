fabe16d81d7a127a374579db9b19165e90e29799
Lauren Hart <laurenannehart@gmail.com>
2018-10-04 14:05:00 +1300

    added user seeds

--- seeds/products.js
+++ seeds/products.js
@@ -4,8 +4,94 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       // Inserts seed entries
       return knex('products').insert([
-        {id: 33301, product_name: 'Oranges', product_description: 'Juicy and ripe!', price: 'Free', quantity: '250 oranges', category: 'Fruit', organic: 1, freerange: 0, user_id: '11101'},
-        {id: 33302, product_name: 'Lemons', product_description: 'Ugly but declicious', price: '$3 per bag', quantity: '50 lemons', category: 'Fruit', organic: 1, freerange: 0, user_id: '11102'}
+        {
+          category: 'Fruit',
+          freerange: '0',
+          id: '33301',
+          organic: '1',
+          price: 'Free',
+          product_description: 'Juicy and ripe!',
+          product_name: 'Oranges',
+          quantity: '250 oranges',
+          user_id: '11101'
+        },
+        {
+          category: 'Fruit',
+          freerange: '0',
+          id: '33302',
+          organic: '1',
+          price: '$3 per bag',
+          product_description: 'Ugly but declicious',
+          product_name: 'Lemons',
+          quantity: '50 lemons',
+          user_id: '11102'
+        },
+        {
+          category: '',
+          freerange: '0',
+          id: '33303',
+          organic: '1',
+          price: '$1 a head',
+          product_description: "How would you describe broccoli? It's green...",
+          product_name: 'Broccoli',
+          quantity: 'too many heads to count!',
+          user_id: '11103'
+        },
+        {
+          category: '',
+          freerange: '0',
+          id: '33304',
+          organic: '1',
+          price: '$2 a bunch',
+          product_description: 'Ripe, great in a Sunday roast! ',
+          product_name: 'Silverbeet',
+          quantity: 'Heaps',
+          user_id: '11103'
+        },
+        {
+          category: '',
+          freerange: '0',
+          id: '33305',
+          organic: '1',
+          price: '$2 a head',
+          product_description: 'Medium sized and leafy',
+          product_name: 'Lettuce',
+          quantity: 'A dozen',
+          user_id: '11104'
+        },
+        {
+          category: '',
+          freerange: '0',
+          id: '33306',
+          organic: '1',
+          price: '50c',
+          product_description: 'Leafy and fragrant',
+          product_name: 'Coriander',
+          quantity: '4 bunches',
+          user_id: '11105'
+        },
+        {
+          category: '',
+          freerange: '0',
+          id: '33307',
+          organic: '1',
+          price: '$3 a dozen',
+          product_description: 'Juicy and vibrant',
+          product_name: 'Peaches',
+          quantity: '30 peaches',
+          user_id: '11106'
+        },
+        {
+          category: '',
+          freerange: '0',
+          id: '33308',
+          organic: '0',
+          price: '$3 for 3',
+          product_description: 'Hot!',
+          product_name: 'Birds Eye Chilis',
+          quantity: '15 chillis',
+          user_id: '11107'
+        }
       ])
     })
 }
--- seeds/users.js
+++ seeds/users.js
@@ -4,8 +4,104 @@ exports.seed = (knex, Promise) => {
     .then(() => {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 11101, isSeller: 1, name: 'Joe Banks', email: 'joeb@yahoo.com', hash: 'jsahfksfbe839afhap9fdfbsdeih9w', lat: -36.8019299, long: 174.7766424, address: '56 Jutland Road', suburb: 'Hauraki', city: 'Auckland', description: 'Retired keen green finger from the sunny Northshore.', hours: '9am - 3pm'},
-        {id: 11102, isSeller: 1, name: 'Mon Collins', email: 'mon@gmail.com', hash: 'jsahfksfbe839afhap9fdfbsdeih9w', lat: -36.7961868, long: 174.7806733, address: '23 Arthur Crescent', suburb: 'Hauraki', city: 'Auckland', description: 'Kids have moved out of home so I have lots of produce to share!', hours: 'Saturday 9am - 12pm'}
+        {
+          address: '56 Jutland Road',
+          city: 'Auckland',
+          description: 'Retired keen green finger from the sunny Northshore.',
+          email: 'joeb@yahoo.com',
+          hash: 'jsahfksfbe839afhap9fdfbsdeih9w',
+          hours: '9am - 3pm',
+          id: '11101',
+          isSeller: '1',
+          lat: '-36.8019299',
+          long: '174.7766424',
+          name: 'Joe Banks',
+          suburb: 'Hauraki'
+        },
+        {
+          address: '23 Arthur Crescent',
+          city: 'Auckland',
+          description: 'Kids have moved out of home so I have lots of produce to share!',
+          email: 'mon@gmail.com',
+          hash: 'jsahfksfbe839afhap9fdfbsdeih9w',
+          hours: 'Saturday 9am - 12pm',
+          id: '11102',
+          isSeller: '1',
+          lat: '-36.7961868',
+          long: '174.7806733',
+          name: 'Mon Collins',
+          suburb: 'Hauraki'
+        },
+        {
+          address: '243 Lake Road',
+          city: 'Auckland',
+          description: 'Hey! I have a thriving veggie patch and would love to share with the community',
+          email: 'dchapman@gmail.com',
+          hash: '$argon2id$v=19$m=8,t=2,p=1$SWgDZz8QbOTM9G7vaa+qOQ$s64POjOCRSWBe2+eCl23SfVtQ4NM0eX5KdeXUSl5oHs',
+          hours: 'Sundays anytime',
+          id: '11103',
+          isSeller: '1',
+          lat: '-36.80195',
+          long: '174.78688',
+          name: 'Danny Chapman',
+          suburb: 'Belmont'
+        },
+        {
+          address: '32 Egremont Street',
+          city: 'Auckland',
+          description: 'Passionate about sustainable living, keen to meet other like minded people',
+          email: 'sarahj@hotmail.com',
+          hash: '$argon2id$v=19$m=8,t=2,p=1$ETKVJJlecmd3SDvtbNcCdA$d2KWLvjS1r7imy1pe/2cTBZIRpMnjRVm/1Ym0rjOraY',
+          hours: 'Saturdays all day',
+          id: '11104',
+          isSeller: '1',
+          lat: '-36.80797',
+          long: '174.78613',
+          name: 'Sarah Jayne',
+          suburb: 'Belmont'
+        },
+        {
+          address: '24 Westwell Road',
+          city: 'Auckland',
+          description: 'Student with a herb garden...',
+          email: 'monique.collins@gmail.com',
+          hash: '$argon2id$v=19$m=8,t=2,p=1$S1X9smMquXF4K692JdIWLA$zQgX2trFre4WApNmkmq0LT438Vh32K221KgSjl+aP0c',
+          hours: 'Saturday Afternoons',
+          id: '11105',
+          isSeller: '1',
+          lat: '-36.80472',
+          long: '174.79239',
+          name: 'Monique Collins',
+          suburb: 'Belmont'
+        },
+        {
+          address: '11 High Street',
+          city: 'Auckland',
+          description: 'Retired, gardening enthusiast!',
+          email: 'dcarter@hotmail.com',
+          hash: '$argon2id$v=19$m=8,t=2,p=1$xPreqEgvkiJZ0SnQ1oQ6Iw$ITkFpH/8pw+jGVNiV9Viop25LKqDjUl0VhNDqxg3S+w',
+          hours: 'Thursday Mornings',
+          id: '11106',
+          isSeller: '1',
+          lat: '-36.82742',
+          long: '174.79537',
+          name: 'Douglas Carter',
+          suburb: 'Devonport'
+        },
+        {
+          address: '80 Eversleigh Road',
+          city: 'Auckland',
+          description: 'Software Developer, I enjoy growing chillis',
+          email: 'zaine@gmail.com',
+          hash: '$argon2id$v=19$m=8,t=2,p=1$IWxo6dfptwfugPTtozJ8zQ$XkeW9Jwp/ct0OsrOJfU6FzrIF52ZX64Wt1L6mphcyCg',
+          hours: 'Saturday 12pm - 3pm',
+          id: '11107',
+          isSeller: '1',
+          lat: '-36.8056',
+          long: '174.78251',
+          name: 'Zaine Kingi',
+          suburb: 'Belmont'
+        }
       ])
     })
 }

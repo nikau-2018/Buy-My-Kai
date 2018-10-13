eb2de72846db3d6f8c41ba7d142fa7346202fcb7
emma-nio <emmanioashley@gmail.com>
2018-10-13 16:36:22 +1300

    added another test which tests whether the getUser function can retrieve a grower's information and products based on their id.

--- tests/db.test.js
+++ tests/db.test.js
@@ -16,16 +16,16 @@ afterEach(() => {
   testEnv.cleanup(testDb)
 })
 
-// test('addUser adds a new user', () => {
-//   const expected = 13
-//   const user = {name: 'Emma', hash: 'emma'}
-//   return db.addUser(user, testDb)
-//     .then(() => { return testDb('users').select() })
-//     .then(results => {
-//       const actual = results.length
-//       expect(actual).toBe(expected)
-//     })
-// })
+test('addUser adds a new user', () => {
+  const expected = 7
+  const user = {name: 'Emma', hash: 'emma'}
+  return db.addUser(user, testDb)
+    .then(() => { return testDb('users').select() })
+    .then(results => {
+      const actual = results.length
+      expect(actual).toBe(expected)
+    })
+})
 
 test('add a product to the database', () => {
   const expected = 13
@@ -69,6 +69,16 @@ test('prod length for one more user', () => {
     })
 })
 
+test('login user', () => {
+  const expected = 'Joe Banks'
+  const userId = 11101
+  return db.getUser(userId, testDb)
+    .then(results => {
+      const actual = results.name
+      expect(actual).toEqual(expected)
+    })
+})
+
 test('gets a user by their id', () => {
   const expected = 'Joe Banks'
   const userId = 11101
@@ -79,7 +89,7 @@ test('gets a user by their id', () => {
     })
 })
 
-test('returns growers by suburb', () => {
+test('returns all growers by suburb', () => {
   const userId = 11106
   const suburb = 'Devonport'
   const expected = [11106]
@@ -88,6 +98,64 @@ test('returns growers by suburb', () => {
     .then(() => { return testDb('users').select().where('users.id', userId) })
     .then(results => {
       const actual = results.map(result => result.id)
+      expect(actual).toEqual(expected)
+    })
+})
+
+test('gets a growers profile information and products using their id', () => {
+  const userId = 11103
+  const expected = [ {id: 33304,
+    isSeller: 1,
+    name: 'Danny Chapman',
+    email: 'dchapman@gmail.com',
+    hash:
+     '$argon2id$v=19$m=8,t=2,p=1$SWgDZz8QbOTM9G7vaa+qOQ$s64POjOCRSWBe2+eCl23SfVtQ4NM0eX5KdeXUSl5oHs',
+    lat: -36.80195,
+    long: 174.78688,
+    address: '243 Lake Road',
+    suburb: 'Belmont',
+    city: 'Auckland',
+    description:
+     'Hey! I have a thriving veggie patch and would love to share with the community',
+    hours: 'Sundays anytime',
+    product_name: 'Strawberry',
+    price: '$2 a bag',
+    quantity: '200',
+    product_description: 'Pick your own.',
+    category: 'Fruit',
+    organic: 1,
+    freerange: 0,
+    user_id: 11103},
+  {id: 33305,
+    isSeller: 1,
+    name: 'Danny Chapman',
+    email: 'dchapman@gmail.com',
+    hash:
+     '$argon2id$v=19$m=8,t=2,p=1$SWgDZz8QbOTM9G7vaa+qOQ$s64POjOCRSWBe2+eCl23SfVtQ4NM0eX5KdeXUSl5oHs',
+    lat: -36.80195,
+    long: 174.78688,
+    address: '243 Lake Road',
+    suburb: 'Belmont',
+    city: 'Auckland',
+    description:
+     'Hey! I have a thriving veggie patch and would love to share with the community',
+    hours: 'Sundays anytime',
+    product_name: 'Cherry',
+    price: '$5 a bag',
+    quantity: '50',
+    product_description: 'Sweet and can be eaten raw, stewed, or in tarts and cakes.',
+    category: 'Fruit',
+    organic: 1,
+    freerange: 0,
+    user_id: 11103} ]
+
+  return db.getSeller(userId, testDb)
+    .then(() => {
+      return testDb('users').join('products', userId, 'products.user_id')
+        .select().where('users.id', userId)
+    })
+    .then(results => {
+      const actual = results
       expect(actual).toEqual(expected)
     })
 })

e067ae3c26b9bf41d0ae57bf69c240d0870a3985
emma-nio <emmanioashley@gmail.com>
2018-10-15 19:31:46 +1300

    created an aboutus component

---
+++ client/src/components/Nav/Aboutus.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AboutUs (props) {
+  return (
+    <div className="pure-menu pure-menu-horizontal">
+      <ul className="pure-menu-list">
+        <li className="pure-menu-item"><a href="/about" className="pure-menu-link"><button>THE TEAM</button></a></li>
+        <li className="pure-menu-item"><a href="/contact" className="pure-menu-link"><button>CONTACT</button></a></li>
+      </ul>
+    </div>
+  )
+}

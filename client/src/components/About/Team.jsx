027afadb7ebefcc0f6bcaff4a1531aa02b0cc0ba
emma-nio <emmanioashley@gmail.com>
2018-10-20 20:47:39 +1300

    added some stuff to the team page

--- client/src/components/About/Team.jsx
+++ client/src/components/About/Team.jsx
@@ -3,25 +3,19 @@ import {Link} from 'react-router-dom'
 import '../../styles/styles.css'
 
 import logo from '../../images/logo-4.png'
-import Nav from '../Nav/Nav'
 
 class Team extends React.Component {
   render () {
     return (
-      <div className="profile">
+      <div className="about">
         <div className="header">
           <Link to='/'>
-            <img className="pure-img logo" src={logo} />
+            <img className="pure-img logo-about" src={logo} />
           </Link>
-          <div className='nav-bar'>
-            <Nav />
-          </div>
         </div>
         <div className='container pure-u-1'>
-          <div className="pure-img background-profile"></div>
-        </div>
-        <div className='pure-u-1'>
-          <a href="#top"><h4> Return to top <i className="fas fa-caret-up"></i></h4></a>
+          <div className="pure-img background-about"></div>
+          <h2>The Team</h2>
         </div>
       </div>
     )
